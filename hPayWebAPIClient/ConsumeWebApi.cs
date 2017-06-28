26c2c39ff329294bd58d5270a0b37bedb38bc822
Manoj Sitapara <manojsitapara@users.noreply.github.com>
2017-06-28 14:43:18 +0530

    Add project files.

---
+++ hPayWebAPIClient/ConsumeWebApi.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hPayWebAPIClient
+{
+    public static class ConsumeWebApi
+    {
+        public static async void GetInvoices(string userId)
+        {
+            using (var client = new HttpClient()) 
+            {
+                var response = client.GetAsync("http://localhost:52795/api/v1/Invoice/GetInvoices/" + userId).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                }
+            }
+        }
+
+        public static async void GetInvoiceStatus(string userId, string invoiceId)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync("http://localhost:52795/api/v1/Invoice/GetInvoiceStatus/" + userId + "/Invoice/" + invoiceId).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                }
+            }
+        }
+
+
+
+        public static async void GetPayment(string userId)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync("http://localhost:52795/api/v1/Payment/GetPayments/" + userId).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                }
+            }
+
+        }
+
+
+
+        public static async void GetInvoicePayment(string userId, string invoiceId)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync("http://localhost:52795/api/v1/Payment/GetInvoicePayments/" + userId + "/Invoice/" + invoiceId).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                }
+            }
+
+        }
+
+
+        public static async void GetInvoiceDetails(string userId, string invoiceId)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync("http://localhost:52795/api/v1/Invoice/GetInvoiceDetails/" + userId + "/Invoice/" + invoiceId).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                }
+            }
+
+        }
+
+
+        public static async void GetInvoiceBalance(string userId, string invoiceId)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync("http://localhost:52795/api/v1/Invoice/GetInvoiceBalance/" + userId + "/Invoice/" + invoiceId).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                }
+            }
+
+        }
+
+
+
+        public static async void RecordPayment(int userId, int invoiceId, bool isDirectPaymentToProvider, decimal paymentAmt, int paymentModeId)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var recordPaymentModel = new RecordPaymentModel
+                {
+                    InvoiceId = invoiceId,
+                    IsDirectPaymentToProvider = isDirectPaymentToProvider,
+                    PaymentAmt = paymentAmt,
+                    PaymentModeId = paymentModeId,
+                    UserId = userId
+                };
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("http://localhost:52795/api/v1/Payment/RecordPayment", recordPaymentModel);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                }
+            }
+
+        }
+
+        class RecordPaymentModel
+        {
+            public int UserId { get; set; }
+            public int InvoiceId { get; set; }
+            public bool IsDirectPaymentToProvider { get; set; }
+            public decimal PaymentAmt { get; set; }
+            public int PaymentModeId { get; set; }
+        }
+
+    }
+}
---
+++ hPayWebAPIClient/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hPayWebAPIClient
+{
+    class Program
+    {
+        public static string userId;
+        public static string invoiceId;
+        static void Main(string[] args)
+        {
+            Console.Write("Please select web API to run");
+            Console.WriteLine(Environment.NewLine);
+            Console.Write("1.GetInvoices");
+            Console.WriteLine(Environment.NewLine);
+            Console.Write("2.GetInvoiceStatus");
+            Console.WriteLine(Environment.NewLine);
+            Console.Write("3.GetInvoiceBalance");
+            Console.WriteLine(Environment.NewLine);
+            Console.Write("4.GetInvoicePayment");
+            Console.WriteLine(Environment.NewLine);
+            Console.Write("5.GetPayments");
+            Console.WriteLine(Environment.NewLine);
+            Console.Write("6.RecordPayment");
+            Console.WriteLine(Environment.NewLine);
+            Console.Write("7.GetInvoiceDetails");
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Enter option number from above: ");
+            string caseNumber = Console.ReadLine();
+            ExecuteTask(caseNumber);
+
+
+
+        }
+
+
+        public static void ExecuteTask(string caseNumber)
+        {
+            if (caseNumber == "1")
+            {
+                Console.WriteLine("Enter userId: ");
+                userId = Console.ReadLine();
+                ConsumeWebApi.GetInvoices(userId);
+            }
+
+            if (caseNumber == "2")
+            {
+                Console.WriteLine("Enter userId: ");
+                userId = Console.ReadLine();
+                Console.WriteLine("Enter InvoiceId: ");
+                invoiceId = Console.ReadLine();
+                ConsumeWebApi.GetInvoiceStatus(userId, invoiceId);
+            }
+
+
+            if (caseNumber == "3")
+            {
+                Console.WriteLine("Enter UserId: ");
+                userId = Console.ReadLine();
+                Console.WriteLine("Enter InvoiceId: ");
+                invoiceId = Console.ReadLine();
+                ConsumeWebApi.GetInvoiceBalance(userId, invoiceId);
+            }
+
+            if (caseNumber == "4")
+            {
+                Console.WriteLine("Enter UserId: ");
+                userId = Console.ReadLine();
+                Console.WriteLine("Enter InvoiceId: ");
+                invoiceId = Console.ReadLine();
+                ConsumeWebApi.GetInvoicePayment(userId, invoiceId);
+            }
+
+            if (caseNumber == "5")
+            {
+                Console.WriteLine("Enter UserId: ");
+                userId = Console.ReadLine();
+                ConsumeWebApi.GetPayment(userId);
+            }
+
+            if (caseNumber == "6")
+            {
+                Console.WriteLine("Enter UserId: ");
+                userId = Console.ReadLine();
+                Console.WriteLine("Enter invoiceId: ");
+                invoiceId = Console.ReadLine();
+                Console.WriteLine("Enter isDirectPaymentToProvider (it should be true or false): ");
+                string isDirectPaymentToProvider = Console.ReadLine();
+                Console.WriteLine("Enter paymentAmt: ");
+                string paymentAmt = Console.ReadLine();
+                Console.WriteLine("Enter paymentModeId: ");
+                string paymentModeId = Console.ReadLine();
+
+                ConsumeWebApi.RecordPayment(Convert.ToInt32(userId), Convert.ToInt32(invoiceId), Convert.ToBoolean(isDirectPaymentToProvider), Convert.ToDecimal(paymentAmt), Convert.ToInt32(paymentModeId));
+            }
+
+            if (caseNumber == "7")
+            {
+                Console.WriteLine("Enter UserId: ");
+                userId = Console.ReadLine();
+                Console.WriteLine("Enter InvoiceId: ");
+                invoiceId = Console.ReadLine();
+                ConsumeWebApi.GetInvoiceDetails(userId, invoiceId);
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
